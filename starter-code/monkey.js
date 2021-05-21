38290252b0c5bba856c25485232b1f6fb1a22655
Mike <83792605+tor-2021@users.noreply.github.com>
2021-05-21 09:22:57 -0400

    Add files via upload

---
+++ starter-code/monkey.js
@@ -0,0 +1,17 @@
+console.log("monkey.js loaded");
+/*Work with a partner to create a monkey object, which has the following properties:
+
+* name
+* species
+* foodsEaten
+
+And the following methods:
+* eatSomething(thingAsString)
+* introduce: produces a string introducing itself, including its name, species, and what it's eaten.
+
+Create 3 monkeys total. Make sure all 3 monkeys have all properties set and methods defined.
+
+Exercise your monkeys by retrieving their properties and using their methods. Practice using both syntaxes
+for retrieving properties (dot notation and brackets).
+
+*/
---
+++ starter-code/objects/app.js
@@ -0,0 +1,99 @@
+/////////////////////////////////////////////////////////////
+
+///////////////////////// Challenge /////////////////////////
+
+/////////////////////////////////////////////////////////////
+
+// Write your answers inside this file, at the places where it's indicated by the comments.
+
+// 1. Suppose that we wanted to create a browser-based calendar program for keeping track of things. 
+//    In comments, list at least three reasonable abstractions that you might use to build this program; 
+//    for each abstraction, list out at least two properties and behaviors that it would make sense for that abstraction to have. 
+//    Below is an example - please write your answer in the same format.
+  
+// 'Car'
+  // Description: Our app manages car sales.
+  // Every 'car' has
+  // - a make
+  // - a model
+  // - a year
+  // Every 'car' can
+  // - drive
+  // - brake
+  // - park
+
+// ANSWER STARTS HERE
+
+// ANSWER ENDS HERE
+
+
+
+
+
+// 2. Create an Object literal that lines up with the following description. 
+//    Store it in the variable 'pet_owner', below. 
+//    Be sure to give it reasonable values for each of its properties.
+
+  // 'Owner'
+  // Description: We are making an app for a veterinary clinic - it manages pets' vaccinations.
+  // Every 'owner' has:
+  // - a name
+  // - an address
+
+  
+  // ANSWER STARTS HERE
+  var pet_owner;
+
+// ANSWER ENDS HERE
+
+
+
+
+
+
+
+
+// 3. Create an Object literal that lines up with the following description. 
+//    Store it in the variable `some_pet`, below.
+
+  // Pet
+  // Description: We are making an app for a veterinary clinic - it manages pets' vaccinations.
+  // Every 'pet' has:
+  // - a name
+  // - a species
+  // - a breed
+  // - a noise that it can make (e.g. 'bark', 'meow', etc.)
+  // Every pet can:
+  // - make noise (each pet makes its own unique noise, as specified by `noise`.
+
+  
+  // ANSWER STARTS HERE
+  var some_pet;
+
+// ANSWER ENDS HERE
+
+
+
+
+
+
+// 4. Create a constructor function for Pet objects, as described above. 
+//    The constructor function should be responseible for setting
+//    the values of `name`, `species`, `breed`, and `noise`. 
+//    The constructor function should also give each pet a method called `makeNoise`,
+//    which behaves as described in Q3.
+
+// ANSWER STARTS HERE
+var Pet;
+
+// ANSWER ENDS HERE
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+
+// You're done! Refer back to README.md.
+
+/////////////////////////////////////////////////////////////
